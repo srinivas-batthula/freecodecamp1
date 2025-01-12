3e6902415ec7800474d94153f264635bc868cbae
Srinivas Batthula <srinivasbatthula05.official@gmail.com>
2025-01-12 21:52:55 +0530

    0.0.3

--- index.js
+++ index.js
@@ -1,42 +1,90 @@
-// index.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
-var express = require('express');
-var app = express();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+const app = express();
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+// Basic Configuration
+const port = process.env.PORT || 3000;
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/public', express.static(`${process.cwd()}/public`));
+
+app.get('/', function(req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
 });
 
-app.get('/api/whoami', (req, res) => {
-  const ip = req.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
-  const language = req.headers['accept-language'];
-  const software = req.headers['user-agent'];
-
-  res.json({
-    ipaddress: ip,
-    language: language,
-    software: software
-  });
-});
-
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
+// Your first API endpoint
+app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+
+// ----------------->>>>>>>>>>>>>>>>>>
+const dns = require('dns');
+
+// In-memory storage for URLs
+const urlDatabase = {};
+let shortUrlCounter = 1;
+
+// Function to validate hostname using DNS lookup
+function validateUrl(submittedUrl, callback) {
+  let hostname;
+
+  try {
+    hostname = new URL(submittedUrl).hostname;
+  } catch (error) {
+    return callback(false);
+  }
+
+  dns.lookup(hostname, (err) => {
+    if (err) {
+      callback(false);
+    } else {
+      callback(true);
+    }
+  });
+}
+
+// Handle POST request to create short URL
+app.post('/api/shorturl', (req, res) => {
+  const originalUrl = req.body.url;
+
+  validateUrl(originalUrl, (isValid) => {
+    if (!isValid) {
+      return res.json({ error: 'invalid url' });
+    }
+
+    // Store URL and assign short URL
+    const shortUrl = shortUrlCounter++;
+    urlDatabase[shortUrl] = originalUrl;
+
+    res.json({
+      original_url: originalUrl,
+      short_url: shortUrl
+    });
+  });
+});
+
+// Redirect to original URL when short URL is visited
+app.get('/api/shorturl/:short_url', (req, res) => {
+  const shortUrl = req.params.short_url;
+  const originalUrl = urlDatabase[shortUrl];
+
+  if (originalUrl) {
+    res.redirect(originalUrl);
+  } else {
+    res.send('Not found');
+  }
+});
+
+app.use('*', (req, res)=>{
+  return res.send('Not found');
+})
+
+app.listen(port, function() {
+  console.log(`Listening on port ${port}`);
 });
